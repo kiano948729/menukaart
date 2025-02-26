e2780c80eace68d03f486d150c870178f1993efd
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2025-02-26 13:08:18 +0100

    checkout-buttons gemaakt/ iccontjes toegevoegd bij de items sectoren

--- menu.php
+++ menu.php
@@ -4,6 +4,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <?php
 include 'categories.php'; // Laad de tijdelijke data
 ?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -49,7 +50,7 @@ include 'categories.php'; // Laad de tijdelijke data
                                 <div class="catogories-div">
                                     <div class="breakfast-main">
                                         <div class="items-catogorie-align">
-                                            <i class="fa-solid fa-mug-hot"></i>
+                                            <i class="fa-solid fa-mug-hot" style="font-size: 40px;"></i>
                                             <h1>Breakfast</h1>
                                             <?php foreach ($categories as $category): ?>
                                                 <?php if ($category['name'] === "Breakfast"): ?>
@@ -60,39 +61,86 @@ include 'categories.php'; // Laad de tijdelijke data
                                     </div>
                                     <div class="soups-main">
                                         <div class="items-catogorie-align">
-
+                                            <img src="img/gif/Ontwerp zonder titel (25).gif" alt="">
+                                            <h1>Soups</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Soups"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
                                         </div>
                                     </div>
                                     <div class="pasta-main">
-                                        <div class="items-catogorie-align">
-
+                                        <div class="items-catogorie-align-boxI">
+                                            <img src="img/gif/Ontwerp zonder titel (26).gif" alt="">
+                                            <h1>Pasta</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Pasta"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
                                         </div>
                                     </div>
                                     <div class="sushi-main">
                                         <div class="items-catogorie-align">
-
+                                            <img src="img/gif/Ontwerp zonder titel (27).gif" alt="">
+                                            <h1>Sushi</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Sushi"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="catogories-div">
                                     <div class="soups-main">
-
+                                        <div class="items-catogorie-align-boxI">
+                                            <img src="img/gif/Ontwerp zonder titel (28).gif" alt="">
+                                            <h1>Maincourse</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Main course"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        </div>
                                     </div>
                                     <div class="breakfast-main">
-
+                                        <div class="items-catogorie-align">
+                                            <img src="img/gif/Ontwerp zonder titel (29).gif" alt="">
+                                            <h1>Desserts</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Desserts"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        </div>
                                     </div>
                                     <div class="soups-main">
-
+                                        <div class="items-catogorie-align-boxI">
+                                            <img src="img/gif/Ontwerp zonder titel (30).gif" alt="">
+                                            <h1>Drinks</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Drinks"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        </div>
                                     </div>
                                     <div class="sushi-main">
-
+                                        <div class="items-catogorie-align-boxI">
+                                            <img src="img/gif/Ontwerp zonder titel (31).gif" alt="">
+                                            <h1>Alcohol</h1>
+                                            <?php foreach ($categories as $category): ?>
+                                                <?php if ($category['name'] === "Alcohol"): ?>
+                                                    <p><?php echo $category['item_count']; ?> items</p>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <hr>
-                            <div class="menu-container">
-                               
-                            </div>
                         </div>
                     </div>
                     <div id="fourth" class="tabcontentWork">
@@ -105,7 +153,45 @@ include 'categories.php'; // Laad de tijdelijke data
             </main>
         </div>
         <div class="menu-table">
+            <div class="menu-table-title">
+                <h1>Table</h1><!-- hier moet nog een id komen voor de user die een table krijgt toegewezen -->
+                <h2>user huppidup</h2><!-- hier moet de naam van de username nog komen -->
+            </div>
+            <div class="menu-table-checkout">
+                <div class="menu-checkout">
+                    <div class="item-check">
 
+                    </div>
+                    <div class="item-payment">
+                        <h2>Payement Method</h2>
+                        <div class="payement-buttons">
+                            <div class="payement-button">
+                                
+                            </div>
+                            <div class="payement-button">
+
+                            </div>
+                            <div class="payement-button">
+
+                            </div>
+                        </div>
+                        <div class="payement-buttons">
+                            <div>
+
+                            </div>
+                            <div>
+
+                            </div>
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-button">
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
