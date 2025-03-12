30a6cea6b959908e65faf18dffff0705cb8f95dc
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2025-03-12 14:01:41 +0100

    alle catogorien zijn aangevuld+ dockerfukes toegevoegd + error gefixt met desserts
    error gefix met dessert

--- menu.php
+++ menu.php
@@ -52,8 +52,8 @@ include 'categories.php';
                     <div class="menu-height">
                         <div class="menu-main-container">
                             <div class="catogories-div">
-                                <div class="breakfast-main">
-                                    <div class="items-catogorie-align">
+                                <div class="breakfast-main" onclick="openCategory('breakfast')">
+                                    <div class="items-catogorie-align-boxI">
                                         <i class="fa-solid fa-mug-hot" style="font-size: 40px;"></i>
                                         <h1>Breakfast</h1>
                                         <?php foreach ($categories as $category): ?>
@@ -63,8 +63,8 @@ include 'categories.php';
                                         <?php endforeach; ?>
                                     </div>
                                 </div>
-                                <div class="soups-main">
-                                    <div class="items-catogorie-align">
+                                <div class="soups-main" onclick="openCategory('soups')">
+                                    <div class="items-catogorie-align-boxI">
                                         <img src="img/gif/Ontwerp zonder titel (25).gif" alt="">
                                         <h1>Soups</h1>
                                         <?php foreach ($categories as $category): ?>
@@ -74,7 +74,7 @@ include 'categories.php';
                                         <?php endforeach; ?>
                                     </div>
                                 </div>
-                                <div class="pasta-main">
+                                <div class="pasta-main" onclick="openCategory('pasta')">
                                     <div class="items-catogorie-align-boxI">
                                         <img src="img/gif/Ontwerp zonder titel (26).gif" alt="">
                                         <h1>Pasta</h1>
@@ -85,8 +85,8 @@ include 'categories.php';
                                         <?php endforeach; ?>
                                     </div>
                                 </div>
-                                <div class="sushi-main">
-                                    <div class="items-catogorie-align">
+                                <div class="sushi-main" onclick="openCategory('sushi')">
+                                    <div class="items-catogorie-align-boxI">
                                         <img src="img/gif/Ontwerp zonder titel (27).gif" alt="">
                                         <h1>Sushi</h1>
                                         <?php foreach ($categories as $category): ?>
@@ -98,7 +98,7 @@ include 'categories.php';
                                 </div>
                             </div>
                             <div class="catogories-div">
-                                <div class="soups-main">
+                                <div class="soups-main" onclick="openCategory('main course')">
                                     <div class="items-catogorie-align-boxI">
                                         <img src="img/gif/Ontwerp zonder titel (28).gif" alt="">
                                         <h1>Maincourse</h1>
@@ -148,10 +148,100 @@ include 'categories.php';
                         <hr>
                         <div class="catogories-to-items">
                             <!-- hier moet de items van de catorgie komen -->
-                            <div id="alcoholItems" class="item-section" style="display: none;">
+                            <div id="breakfastItems" class="item-section" style="display: none;">
                                 <div class="items-grid">
                                     <?php foreach ($categories as $category): ?>
-                                        <?php if ($category['name'] === "Alcohol"): ?>
+                                        <?php if ($category['name'] === "Breakfast"): ?>
+                                            <div class="category-section" id="alcoholItems">
+                                                <h2><?php echo $category['name']; ?></h2>
+                                                <!-- Grid container voor producten -->
+                                                <div class="items-grid">
+                                                    <?php foreach ($category['items'] as $item): ?>
+                                                        <div class="item">
+                                                            <div class="item-details">
+                                                                <p>Product: <strong><?php echo $item['name']; ?></strong></p>
+                                                                <p>Prijs: €<?php echo number_format($item['price'], 2); ?></p>
+                                                                <p>Stock: <span class="item-stock" data-name="<?php echo $item['name']; ?>">
+                                                                <?php echo $item['stock']; ?></span></p>
+                                                            </div>
+                                                            <!-- Controls voor hoeveelheid -->
+                                                            <div class="item-controls">
+                                                                <button class="decrease-btn" data-name="<?php echo $item['name']; ?>">-</button>
+                                                                <span class="item-quantity" data-name="<?php echo $item['name']; ?>">0</span>
+                                                                <button class="increase-btn" data-name="<?php echo $item['name']; ?>">+</button>
+                                                            </div>
+                                                        </div>
+                                                    <?php endforeach; ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                            <div id="soupsItems" class="item-section" style="display: none;">
+                                <div class="items-grid">
+                                    <?php foreach ($categories as $category): ?>
+                                        <?php if ($category['name'] === "Soups"): ?>
+                                            <div class="category-section" id="alcoholItems">
+                                                <h2><?php echo $category['name']; ?></h2>
+                                                <!-- Grid container voor producten -->
+                                                <div class="items-grid">
+                                                    <?php foreach ($category['items'] as $item): ?>
+                                                        <div class="item">
+                                                            <div class="item-details">
+                                                                <p>Product: <strong><?php echo $item['name']; ?></strong></p>
+                                                                <p>Prijs: €<?php echo number_format($item['price'], 2); ?></p>
+                                                                <p>Stock: <span class="item-stock" data-name="<?php echo $item['name']; ?>">
+                                                                <?php echo $item['stock']; ?></span></p>
+                                                            </div>
+                                                            <!-- Controls voor hoeveelheid -->
+                                                            <div class="item-controls">
+                                                                <button class="decrease-btn" data-name="<?php echo $item['name']; ?>">-</button>
+                                                                <span class="item-quantity" data-name="<?php echo $item['name']; ?>">0</span>
+                                                                <button class="increase-btn" data-name="<?php echo $item['name']; ?>">+</button>
+                                                            </div>
+                                                        </div>
+                                                    <?php endforeach; ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                            <div id="pastaItems" class="item-section" style="display: none;">
+                                <div class="items-grid">
+                                    <?php foreach ($categories as $category): ?>
+                                        <?php if ($category['name'] === "Pasta"): ?>
+                                            <div class="category-section" id="alcoholItems">
+                                                <h2><?php echo $category['name']; ?></h2>
+                                                <!-- Grid container voor producten -->
+                                                <div class="items-grid">
+                                                    <?php foreach ($category['items'] as $item): ?>
+                                                        <div class="item">
+                                                            <div class="item-details">
+                                                                <p>Product: <strong><?php echo $item['name']; ?></strong></p>
+                                                                <p>Prijs: €<?php echo number_format($item['price'], 2); ?></p>
+                                                                <p>Stock: <span class="item-stock" data-name="<?php echo $item['name']; ?>">
+                                                                <?php echo $item['stock']; ?></span></p>
+                                                            </div>
+                                                            <!-- Controls voor hoeveelheid -->
+                                                            <div class="item-controls">
+                                                                <button class="decrease-btn" data-name="<?php echo $item['name']; ?>">-</button>
+                                                                <span class="item-quantity" data-name="<?php echo $item['name']; ?>">0</span>
+                                                                <button class="increase-btn" data-name="<?php echo $item['name']; ?>">+</button>
+                                                            </div>
+                                                        </div>
+                                                    <?php endforeach; ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                            <div id="sushiItems" class="item-section" style="display: none;">
+                                <div class="items-grid">
+                                    <?php foreach ($categories as $category): ?>
+                                        <?php if ($category['name'] === "Sushi"): ?>
                                             <div class="category-section" id="alcoholItems">
                                                 <h2><?php echo $category['name']; ?></h2>
                                                 <!-- Grid container voor producten -->
@@ -179,6 +269,67 @@ include 'categories.php';
                                 </div>
                             </div>
                             <!-- Einde catogorie items van Alcohol -->
+
+                            <div id="main courseItems" class="item-section" style="display: none;">
+                                <div class="items-grid">
+                                    <?php foreach ($categories as $category): ?>
+                                        <?php if ($category['name'] === "Main course"): ?>
+                                            <div class="category-section" id="alcoholItems">
+                                                <h2><?php echo $category['name']; ?></h2>
+                                                <!-- Grid container voor producten -->
+                                                <div class="items-grid">
+                                                    <?php foreach ($category['items'] as $item): ?>
+                                                        <div class="item">
+                                                            <div class="item-details">
+                                                                <p>Product: <strong><?php echo $item['name']; ?></strong></p>
+                                                                <p>Prijs: €<?php echo number_format($item['price'], 2); ?></p>
+                                                                <p>Stock: <span class="item-stock" data-name="<?php echo $item['name']; ?>">
+                                                                <?php echo $item['stock']; ?></span></p>
+                                                            </div>
+                                                            <!-- Controls voor hoeveelheid -->
+                                                            <div class="item-controls">
+                                                                <button class="decrease-btn" data-name="<?php echo $item['name']; ?>">-</button>
+                                                                <span class="item-quantity" data-name="<?php echo $item['name']; ?>">0</span>
+                                                                <button class="increase-btn" data-name="<?php echo $item['name']; ?>">+</button>
+                                                            </div>
+                                                        </div>
+                                                    <?php endforeach; ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                            <div id="dessertsItems" class="item-section" style="display: none;">
+                                <div class="items-grid">
+                                    <?php foreach ($categories as $category): ?>
+                                        <?php if ($category['name'] === "Desserts"): ?>
+                                            <div class="category-section" id="alcoholItems">
+                                                <h2><?php echo $category['name']; ?></h2>
+                                                <!-- Grid container voor producten -->
+                                                <div class="items-grid">
+                                                    <?php foreach ($category['items'] as $item): ?>
+                                                        <div class="item">
+                                                            <div class="item-details">
+                                                                <p>Product: <strong><?php echo $item['name']; ?></strong></p>
+                                                                <p>Prijs: €<?php echo number_format($item['price'], 2); ?></p>
+                                                                <p>Stock: <span class="item-stock" data-name="<?php echo $item['name']; ?>">
+                                                                <?php echo $item['stock']; ?></span></p>
+                                                            </div>
+                                                            <!-- Controls voor hoeveelheid -->
+                                                            <div class="item-controls">
+                                                                <button class="decrease-btn" data-name="<?php echo $item['name']; ?>">-</button>
+                                                                <span class="item-quantity" data-name="<?php echo $item['name']; ?>">0</span>
+                                                                <button class="increase-btn" data-name="<?php echo $item['name']; ?>">+</button>
+                                                            </div>
+                                                        </div>
+                                                    <?php endforeach; ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
                             <div id="drinksItems" class="item-section" style="display: none;">
                                 <div class="items-grid">
                                     <?php foreach ($categories as $category): ?>
@@ -209,10 +360,10 @@ include 'categories.php';
                                     <?php endforeach; ?>
                                 </div>
                             </div>
-                            <div id="dessertItems" class="item-section" style="display: none;">
+                            <div id="alcoholItems" class="item-section" style="display: none;">
                                 <div class="items-grid">
                                     <?php foreach ($categories as $category): ?>
-                                        <?php if ($category['name'] === "Desserts"): ?>
+                                        <?php if ($category['name'] === "Alcohol"): ?>
                                             <div class="category-section" id="alcoholItems">
                                                 <h2><?php echo $category['name']; ?></h2>
                                                 <!-- Grid container voor producten -->
